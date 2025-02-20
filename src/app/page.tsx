df0236eac1b646fea06ed1e8efbd81918581c259
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-20 13:31:57 +0100

    Remove test query on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,45 +11,39 @@ import {
     TransactionPanel,
     TransactionTable,
 } from '../components'
-import { loadBlocks } from '../lib/loaders'
 import styles from './page.module.css'
 
 const latestBlocks = blocks.slice(0, 10)
 const latestTransactions = transactions.slice(0, 10)
 
-const HomePage: FC = async () => {
-    const blocks = await loadBlocks(10)
-    console.log(blocks)
+const HomePage: FC = async () => (
+    <>
+        <Container>
+            <h1 className={styles.title}>Penumbra Blockchain Explorer</h1>
+            <div className={styles.search}>
+                <Search size={16} />
+                <span>Search by address, hash number, blocks, etc.</span>
+            </div>
+        </Container>
+        <Container>
+            <div className={styles.dashboard}>
+                <BlockPanel />
+                <TransactionPanel />
+                <BurnPanel />
+                <BlockTable
+                    actions={<Button href="/blocks">View all</Button>}
+                    blocks={latestBlocks}
+                    title="Latest blocks"
+                />
+                <TransactionTable
+                    actions={<Button href="/transactions">View all</Button>}
+                    title="Latest transactions"
+                    transactions={latestTransactions}
+                />
+            </div>
+        </Container>
+    </>
+)
 
-    return (
-        <>
-            <Container>
-                <h1 className={styles.title}>Penumbra Blockchain Explorer</h1>
-                <div className={styles.search}>
-                    <Search size={16} />
-                    <span>Search by address, hash number, blocks, etc.</span>
-                </div>
-            </Container>
-            <Container>
-                <div className={styles.dashboard}>
-                    <BlockPanel />
-                    <TransactionPanel />
-                    <BurnPanel />
-                    <BlockTable
-                        actions={<Button href="/blocks">View all</Button>}
-                        blocks={latestBlocks}
-                        title="Latest blocks"
-                    />
-                    <TransactionTable
-                        actions={<Button href="/transactions">View all</Button>}
-                        title="Latest transactions"
-                        transactions={latestTransactions}
-                    />
-                </div>
-            </Container>
-        </>
-    )
-}
-
-export const revalidate = 1
+// export const revalidate = 1
 export default HomePage
