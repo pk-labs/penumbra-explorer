471940f48486739597238663e9536ef70d0dc4c5
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-20 15:02:47 +0100

    Use hardcoded GraphQL URL for now

--- src/lib/graphql/createGraphqlClient.ts
+++ src/lib/graphql/createGraphqlClient.ts
@@ -2,12 +2,14 @@
 import { cacheExchange, Client, fetchExchange } from 'urql'
 
 const createGraphqlClient = () => {
-    if (!process.env.NEXT_PUBLIC_GRAPHQL_URL) {
-        throw Error('Missing NEXT_PUBLIC_GRAPHQL_URL')
-    }
+    // if (!process.env.NEXT_PUBLIC_GRAPHQL_URL) {
+    //     throw Error('Missing NEXT_PUBLIC_GRAPHQL_URL')
+    // }
 
     return new Client({
-        url: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+        // TODO: Use env variable once implemented on CI/CD
+        // url: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+        url: 'https://penumbra-explorer.buber.eepy.sh/',
         fetchOptions: {
             credentials: 'omit',
         },
