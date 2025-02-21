59689985cfe7fcf71c74b5fb7bdc722d6e3fc138
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-21 14:20:32 +0100

    Add partial incognito icon to action list

--- src/components/actionList/actionList.tsx
+++ src/components/actionList/actionList.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react'
 import { TransactionFragment } from '../../lib/graphql/generated/types'
 import styles from './actionList.module.css'
 import incognitoFull from './incognitoFull.svg'
+import incognitoPartial from './incognitoPartial.svg'
 
 interface Props {
     actions: TransactionFragment['body']['actions']
@@ -13,17 +14,24 @@ const ActionList: FC<Props> = props => {
         <div className={styles.root}>
             <h3 className={styles.title}>Actions</h3>
             <ul className={styles.list}>
-                {props.actions.map((action, i) => (
-                    <li key={i} className={styles.item}>
-                        <Image
-                            alt={action.__typename}
-                            height={incognitoFull.height}
-                            src={incognitoFull.src}
-                            width={incognitoFull.width}
-                        />
-                        {action.__typename}
-                    </li>
-                ))}
+                {props.actions.map((action, i) => {
+                    const icon =
+                        i < props.actions.length - 1
+                            ? incognitoFull
+                            : incognitoPartial
+
+                    return (
+                        <li key={i} className={styles.item}>
+                            <Image
+                                alt={action.__typename}
+                                height={icon.height}
+                                src={icon.src}
+                                width={icon.width}
+                            />
+                            {action.__typename}
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
