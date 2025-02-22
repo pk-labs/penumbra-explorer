415270185a2e209aaadf75a98c463a1d427e761a
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-22 10:28:34 +0100

    Use .env-cmdrc.json for different environments

--- src/lib/graphql/createGraphqlClient.ts
+++ src/lib/graphql/createGraphqlClient.ts
@@ -2,14 +2,12 @@
 import { cacheExchange, Client, fetchExchange } from 'urql'
 
 const createGraphqlClient = () => {
-    // if (!process.env.NEXT_PUBLIC_GRAPHQL_URL) {
-    //     throw Error('Missing NEXT_PUBLIC_GRAPHQL_URL')
-    // }
+    if (!process.env.GRAPHQL_URL) {
+        throw Error('Missing GRAPHQL_URL')
+    }
 
     return new Client({
-        // TODO: Use env variable once implemented on CI/CD
-        // url: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-        url: 'https://penumbra-explorer.buber.eepy.sh/',
+        url: process.env.GRAPHQL_URL,
         fetchOptions: {
             credentials: 'omit',
         },
