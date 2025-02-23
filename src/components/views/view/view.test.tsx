43ba08e4b1b4b4b0d5622276d8cbef38b7660591
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-23 16:21:12 +0100

    Refactor view tests

--- src/components/views/view/view.test.tsx
+++ src/components/views/view/view.test.tsx
@@ -4,8 +4,8 @@ import View from './view'
 describe('View', () => {
     test('renders children, title and subtitle', async () => {
         const { container } = render(
-            <View subtitle="Baz" title="Bar">
-                Foo
+            <View subtitle="Bar" title="Foo">
+                Baz
             </View>
         )
 
@@ -16,11 +16,17 @@ describe('View', () => {
 
     test('applies custom classes', async () => {
         const { container } = render(
-            <View className="foo" subtitle="Baz" title="Bar">
-                Foo
+            <View className="foo bar" title="Foo">
+                Bar
             </View>
         )
 
-        expect(getByText(container, 'Foo').classList.contains('foo')).toBe(true)
+        expect(container.firstElementChild?.classList.contains('foo')).toBe(
+            true
+        )
+
+        expect(container.firstElementChild?.classList.contains('bar')).toBe(
+            true
+        )
     })
 })
