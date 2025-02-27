f0c271e61fe84384fe2a5455d01f5d7ff089256c
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-27 13:52:42 +0100

    Implement transactions one-way pagination

--- src/app/txs/page.tsx
+++ src/app/txs/page.tsx
@@ -1,5 +1,6 @@
 /* istanbul ignore file */
 import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 import { FC } from 'react'
 import {
     Breadcrumb,
@@ -9,15 +10,44 @@ import {
     TransactionTable,
 } from '@/components'
 import { rootTitle } from '@/lib/constants'
+import { PartialTransactionFragment } from '@/lib/graphql/generated/types'
 import { loadTransactions } from '@/lib/loaders'
 
 export const metadata: Metadata = {
     title: `Transactions - ${rootTitle}`,
 }
 
-const TransactionsPage: FC = async () => {
+interface Props {
+    searchParams: Promise<{
+        from?: string
+    }>
+}
+
+const TransactionsPage: FC<Props> = async props => {
+    const searchParams = await props.searchParams
+    const fromParam = searchParams.from
     const limit = 20
-    const transactions = await loadTransactions({ latest: { limit } })
+    let transactions: PartialTransactionFragment[] | undefined
+    let fromNext: string | undefined
+
+    if (fromParam) {
+        transactions = await loadTransactions({
+            range: { fromTxHash: fromParam.toUpperCase(), limit },
+        })
+
+        if (transactions?.length) {
+            transactions.sort((a, b) => b.block.createdAt - a.block.createdAt)
+            fromNext = transactions[transactions.length - 1].hash
+        } else {
+            redirect('/txs')
+        }
+    } else {
+        transactions = await loadTransactions({ latest: { limit } })
+
+        if (transactions?.length) {
+            fromNext = transactions[transactions.length - 1].hash
+        }
+    }
 
     return (
         <Container>
@@ -26,7 +56,7 @@ const TransactionsPage: FC = async () => {
                 <Breadcrumb>Transactions</Breadcrumb>
             </Breadcrumbs>
             <TransactionTable
-                footer={<Pagination />}
+                footer={<Pagination fromNext={fromNext} />}
                 transactions={transactions}
                 time
             />
