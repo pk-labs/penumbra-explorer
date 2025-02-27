c62601503e0cbd761d0a7c76d3b6a7e2ddfbfe52
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-27 15:25:28 +0100

    Style search input

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -2,7 +2,7 @@
 
 import clsx from 'clsx'
 import { Search as SearchIcon } from 'lucide-react'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useCallback, useRef } from 'react'
 import styles from './search.module.css'
 
 interface Props {
@@ -11,11 +11,25 @@ interface Props {
     narrow?: boolean
 }
 
-const Search: FC<Props> = props => (
-    <div className={clsx(styles.root, props.className)}>
-        <SearchIcon size={16} />
-        <span>Search by address, hash number, blocks, etc.</span>
-    </div>
-)
+const Search: FC<Props> = props => {
+    const input = useRef<HTMLInputElement>(null)
+
+    const focusInput = useCallback(() => input.current?.focus(), [])
+
+    return (
+        <div className={clsx(styles.root, props.className)}>
+            <SearchIcon
+                className={styles.icon}
+                onClick={focusInput}
+                size={16}
+            />
+            <input
+                ref={input}
+                className={styles.input}
+                placeholder="Search by address, hash number, blocks, etc."
+            />
+        </div>
+    )
+}
 
 export default Search
