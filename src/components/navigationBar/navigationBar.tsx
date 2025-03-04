299ef7e524deb93faabcd7015aea49f292265e57
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-04 14:48:06 +0100

    Don't show navigation bar search on home page

--- src/components/navigationBar/navigationBar.tsx
+++ src/components/navigationBar/navigationBar.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import clsx from 'clsx'
 import { Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { FC } from 'react'
 import { logo } from '@/lib/images'
 import { Tab, Tabs } from '../tabs'
@@ -11,34 +14,45 @@ interface Props {
     className?: string
 }
 
-const NavigationBar: FC<Props> = props => (
-    <header className={clsx(styles.root, props.className)}>
-        <div className={styles.group}>
-            <Link href="/">
-                <Image alt="Noctis" height={33} src={logo.src} width={120} />
-            </Link>
-        </div>
-        <Tabs className={styles.tabs}>
-            <Tab href="/">Home</Tab>
-            <Tab href="/blocks" paths={['/block']}>
-                Blocks
-            </Tab>
-            <Tab href="/txs" paths={['/tx']}>
-                Transactions
-            </Tab>
-        </Tabs>
-        <div className={styles.group}>
-            <div className={styles.search}>
-                <Search size={16} />
-                <span>Search</span>
+const NavigationBar: FC<Props> = props => {
+    const pathname = usePathname()
+
+    return (
+        <header className={clsx(styles.root, props.className)}>
+            <div className={styles.group}>
+                <Link href="/">
+                    <Image
+                        alt="Noctis"
+                        height={33}
+                        src={logo.src}
+                        width={120}
+                    />
+                </Link>
             </div>
-            <div className={styles.price}>
-                <span className={styles.label}>UM Price:</span>
-                <span>$0.98</span>
-                <span className={styles.movement}>(+1.1%)</span>
+            <Tabs className={styles.tabs}>
+                <Tab href="/">Home</Tab>
+                <Tab href="/blocks" paths={['/block']}>
+                    Blocks
+                </Tab>
+                <Tab href="/txs" paths={['/tx']}>
+                    Transactions
+                </Tab>
+            </Tabs>
+            <div className={styles.group}>
+                {pathname !== '/' && (
+                    <div className={styles.search}>
+                        <Search size={16} />
+                        <span>Search</span>
+                    </div>
+                )}
+                <div className={styles.price}>
+                    <span className={styles.label}>UM Price:</span>
+                    <span>$0.98</span>
+                    <span className={styles.movement}>(+1.1%)</span>
+                </div>
             </div>
-        </div>
-    </header>
-)
+        </header>
+    )
+}
 
 export default NavigationBar
