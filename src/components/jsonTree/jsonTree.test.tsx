51e36318e739c53ea2dc5b61f8f64f4680fd7ec7
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-10 17:21:10 +0100

    Refactor JSON tree tests

--- src/components/jsonTree/jsonTree.test.tsx
+++ src/components/jsonTree/jsonTree.test.tsx
@@ -7,19 +7,7 @@ import {
 } from '@testing-library/react'
 import JsonTree from './jsonTree'
 
-// jest.mock('@microlink/react-json-view', () => (props: ReactJsonViewProps) => (
-//     <div>{JSON.stringify(props.src)}</div>
-// ))
-
 describe('JsonTree', () => {
-    // test('renders JSON', async () => {
-    //     const { container } = render(<JsonTree data={{ foo: 'bar' }} />)
-    //
-    //     await waitFor(() => {
-    //         expect(container.firstChild).toHaveTextContent('{"foo":"bar"}')
-    //     })
-    // })
-
     test('is collapsed by default', async () => {
         const { container } = render(<JsonTree data={{ foo: 'bar' }} />)
 
