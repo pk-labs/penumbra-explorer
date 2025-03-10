299483228727b38276a0e86deee5f229be3a3d0b
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-10 16:05:04 +0100

    Show JSON tree collapsed and expand to two levels

--- src/components/jsonTree/jsonTree.test.tsx
+++ src/components/jsonTree/jsonTree.test.tsx
@@ -1,17 +1,60 @@
-import { ReactJsonViewProps } from '@microlink/react-json-view'
-import { render, waitFor } from '@testing-library/react'
+import {
+    fireEvent,
+    getByText,
+    queryByText,
+    render,
+    waitFor,
+} from '@testing-library/react'
 import JsonTree from './jsonTree'
 
-jest.mock('@microlink/react-json-view', () => (props: ReactJsonViewProps) => (
-    <div>{JSON.stringify(props.src)}</div>
-))
+// jest.mock('@microlink/react-json-view', () => (props: ReactJsonViewProps) => (
+//     <div>{JSON.stringify(props.src)}</div>
+// ))
 
 describe('JsonTree', () => {
-    test('renders JSON', async () => {
+    // test('renders JSON', async () => {
+    //     const { container } = render(<JsonTree data={{ foo: 'bar' }} />)
+    //
+    //     await waitFor(() => {
+    //         expect(container.firstChild).toHaveTextContent('{"foo":"bar"}')
+    //     })
+    // })
+
+    test('is collapsed by default', async () => {
         const { container } = render(<JsonTree data={{ foo: 'bar' }} />)
 
         await waitFor(() => {
-            expect(container.firstChild).toHaveTextContent('{"foo":"bar"}')
+            getByText(container, 'root')
+            expect(queryByText(container, 'foo')).toBeNull()
+            expect(queryByText(container, 'bar')).toBeNull()
+        })
+    })
+
+    test('expands first two levels on first click', async () => {
+        const { container } = render(
+            <JsonTree
+                data={{
+                    level1: {
+                        level2: {
+                            level3: 'foo',
+                        },
+                    },
+                }}
+            />
+        )
+
+        await waitFor(() => {
+            getByText(container, 'root')
+            expect(queryByText(container, 'level1')).toBeNull()
+        })
+
+        fireEvent.click(getByText(container, 'root'))
+
+        await waitFor(() => {
+            getByText(container, 'root')
+            getByText(container, 'level1')
+            getByText(container, 'level2')
+            expect(queryByText(container, 'level3')).toBeNull()
         })
     })
 })
--- src/components/jsonTree/jsonTree.tsx
+++ src/components/jsonTree/jsonTree.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { FC } from 'react'
+import { FC, useCallback, useState } from 'react'
 import Subsection from '../subsection'
 import styles from './jsonTree.module.css'
 
@@ -13,19 +13,24 @@ interface Props {
     data: object
 }
 
-const JsonTree: FC<Props> = props => (
-    <Subsection title="Raw JSON">
-        <div className={styles.container}>
-            <ReactJsonView
-                collapsed={2}
-                src={props.data}
-                style={{
-                    background: 'transparent',
-                }}
-                theme="twilight"
-            />
-        </div>
-    </Subsection>
-)
+const JsonTree: FC<Props> = props => {
+    const [clicked, setClicked] = useState(false)
+
+    const onClick = useCallback(() => setClicked(true), [])
+
+    return (
+        <Subsection title="Raw JSON">
+            <div className={styles.container} onClick={onClick}>
+                <ReactJsonView
+                    // Expand two levels when expanding for the first time
+                    collapsed={clicked ? 2 : true}
+                    src={props.data}
+                    style={{ background: 'transparent' }}
+                    theme="twilight"
+                />
+            </div>
+        </Subsection>
+    )
+}
 
 export default JsonTree
