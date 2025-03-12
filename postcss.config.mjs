9fb1f9cab2c3c8bfed1ac855b12a12187fc3bfc1
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-12 18:50:54 +0100

    Remove no longer needed postcss-nesting

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
-/* eslint-disable perfectionist/sort-objects */
 export default {
     plugins: {
-        // TODO: Remove postcss-nesting once Tailwind no longer complains about
-        //  unpure selectors used in tables etc.
-        'postcss-nesting': {},
         '@tailwindcss/postcss': {},
     },
 }
