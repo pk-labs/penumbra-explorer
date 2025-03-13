8ebdeb780c96d8f207f1152a8941fd424b41a1a4
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-13 16:46:31 +0100

    Refactor search result to Tailwind CSS

--- src/components/search/searchResult/searchResult.tsx
+++ src/components/search/searchResult/searchResult.tsx
@@ -1,24 +1,36 @@
 'use client'
 
+import clsx from 'clsx'
 import { Box, CheckCheck } from 'lucide-react'
 import Link from 'next/link'
 import { FC } from 'react'
 import { formatNumber } from '@/lib/utils'
-import styles from './searchResult.module.css'
 
 export interface Props {
     heightOrHash: number | string
 }
 
 const SearchResult: FC<Props> = props => (
-    <li className={styles.root}>
+    <li
+        className={clsx(
+            'transition-bg duration-200 ease-(--fastOutSlowIn)',
+            'not-last:border-b-1 not-last:border-b-(--surfaceLighter)',
+            'hover:bg-(--surface)'
+        )}
+    >
         {typeof props.heightOrHash === 'number' ? (
-            <Link className={styles.link} href={`/block/${props.heightOrHash}`}>
+            <Link
+                className="flex h-8 items-center gap-1 py-2 text-(--text)!"
+                href={`/block/${props.heightOrHash}`}
+            >
                 <Box color="var(--textSecondary)" size={16} />
                 {formatNumber(props.heightOrHash)}
             </Link>
         ) : (
-            <Link className={styles.link} href={`/tx/${props.heightOrHash}`}>
+            <Link
+                className="flex h-8 items-center gap-1 py-2 text-(--text)!"
+                href={`/tx/${props.heightOrHash}`}
+            >
                 <CheckCheck color="var(--secondaryLight)" size={16} />
                 {props.heightOrHash}
             </Link>
