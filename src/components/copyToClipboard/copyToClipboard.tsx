e143a64efdaa1642e334d28f41e86cb437960e50
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-14 16:30:43 +0100

    Refactor copy to clipboard

--- src/components/copyToClipboard/copyToClipboard.tsx
+++ src/components/copyToClipboard/copyToClipboard.tsx
@@ -48,8 +48,10 @@ const CopyToClipboard: FC<Props> = props => {
     return (
         <div
             className={clsx(
-                'transition-background inline-flex cursor-pointer items-center justify-center rounded-lg border-1 border-transparent duration-200 ease-(--fastOutSlowIn) hover:bg-(--surfaceLight)',
-                props.small ? 'p-0.5' : 'p-1',
+                'transition-background inline-flex cursor-pointer items-center',
+                'justify-center rounded-lg border-1 border-transparent p-1',
+                'duration-200 ease-(--fastOutSlowIn) hover:bg-(--surfaceLight)',
+                props.small && 'p-0.5',
                 copied && 'animate-copy',
                 props.className
             )}
