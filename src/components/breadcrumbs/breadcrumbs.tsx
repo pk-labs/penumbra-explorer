95f3097c6a6a18b009be98a617a91c2ba7b15b84
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-14 13:46:39 +0100

    Refactor breadcrumbs to Tailwind CSS

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx'
 import { ChevronRight } from 'lucide-react'
 import { Children, FC, ReactElement } from 'react'
 import { BreadcrumbProps } from './breadcrumb'
-import styles from './breadcrumbs.module.css'
 
 interface Props {
     children?:
@@ -21,12 +20,15 @@ const Breadcrumbs: FC<Props> = props => {
     const lastIndex = Children.count(props.children) - 1
 
     return (
-        <nav className={clsx(styles.root, props.className)}>
+        <nav className={clsx('mb-4 flex items-center gap-2', props.className)}>
             {Children.map(props.children, (child, index) => (
                 <>
                     {child}
                     {index < lastIndex && (
-                        <ChevronRight className={styles.separator} size={24} />
+                        <ChevronRight
+                            className="text-(--textSecondary)"
+                            size={24}
+                        />
                     )}
                 </>
             ))}
