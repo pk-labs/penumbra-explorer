10f0e4f173ab2dd0a50dd11314f2a1471e50629a
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-14 14:12:25 +0100

    Refactor memo to Tailwind CSS

--- src/components/memo/memo.tsx
+++ src/components/memo/memo.tsx
@@ -1,13 +1,18 @@
 // istanbul ignore file
+import clsx from 'clsx'
 import Image from 'next/image'
 import { FC } from 'react'
 import { encrypted } from '@/lib/images'
 import Subsection from '../subsection'
-import styles from './memo.module.css'
 
 const Memo: FC = () => (
     <Subsection title="Memo">
-        <div className={styles.content}>
+        <div
+            className={clsx(
+                'flex items-center gap-2 rounded-lg bg-(--surface) px-3 py-2',
+                'text-sm font-normal text-(--textSecondary)'
+            )}
+        >
             <Image alt="Memo" src={encrypted} />
             <span>Memo</span>
         </div>
