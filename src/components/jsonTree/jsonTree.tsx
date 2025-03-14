484c1846a7820d49ee274ad5a63c79395ff6d013
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-14 14:00:50 +0100

    Refactor JSON tree to Tailwind CSS

--- src/components/jsonTree/jsonTree.tsx
+++ src/components/jsonTree/jsonTree.tsx
@@ -1,9 +1,9 @@
 'use client'
 
+import clsx from 'clsx'
 import dynamic from 'next/dynamic'
 import { FC, useCallback, useState } from 'react'
 import Subsection from '../subsection'
-import styles from './jsonTree.module.css'
 
 const ReactJsonView = dynamic(() => import('@microlink/react-json-view'), {
     ssr: false,
@@ -20,7 +20,13 @@ const JsonTree: FC<Props> = props => {
 
     return (
         <Subsection title="Raw JSON">
-            <div className={styles.container} onClick={onClick}>
+            <div
+                className={clsx(
+                    'rounded-lg bg-(--surface) p-3 font-mono text-xs',
+                    'font-medium break-all'
+                )}
+                onClick={onClick}
+            >
                 <ReactJsonView
                     // Expand two levels when expanding for the first time
                     collapsed={clicked ? 2 : true}
