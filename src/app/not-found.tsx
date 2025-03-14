90e08111e068afaf55eb447f461a20c807906924
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-14 11:40:06 +0100

    Refactor not found page to Tailwind CSS

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
 // istanbul ignore file
+import clsx from 'clsx'
 import { FC } from 'react'
-import styles from './page.module.css'
 
 const NotFoundPage: FC = async () => (
-    <div className={styles.notFound}>
-        <span className={styles.status}>404</span>
-        <span className={styles.separator} />
+    <div
+        className={clsx(
+            'absolute top-[50vh] left-[50vw] flex -translate-1/2 items-center',
+            'justify-center gap-6'
+        )}
+    >
+        <span className="font-mono text-3xl font-medium">404</span>
+        <span className="h-12 w-[1px] bg-(--surfaceLighter)" />
         <span>This page could not be found.</span>
     </div>
 )
