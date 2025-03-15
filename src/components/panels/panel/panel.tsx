fc67c8e9b585fcbe8f70854577f429c0ed167a6b
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-15 22:51:18 +0100

    Refactor panel to Tailwind CSS

--- src/components/panels/panel/panel.tsx
+++ src/components/panels/panel/panel.tsx
@@ -3,7 +3,6 @@
 import { FC, ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { NumberCountup } from '../../numberCountup'
-import styles from './panel.module.css'
 
 export interface Props {
     children?: ReactNode
@@ -15,9 +14,24 @@ export interface Props {
 }
 
 const Panel: FC<Props> = props => (
-    <section className={twMerge(styles.root, props.className)}>
-        <header className={styles.header}>
-            <h2 className={styles.title}>{props.title}</h2>
+    <section
+        className={twMerge(
+            'grid-te grid grid-cols-2 grid-rows-1 rounded-2xl',
+            'bg-radial-[100%_100%_at_0%_0%] from-[rgba(174,174,174,0.25)]',
+            'from-0% to-[rgba(174,174,174,0.03)] to-100% p-6',
+            'backdrop-blur-[32px]',
+            props.className
+        )}
+    >
+        <header className="flex flex-col gap-2">
+            <h2
+                className={twMerge(
+                    'flex items-center gap-2 text-base font-medium',
+                    'text-(--textSecondary) capitalize'
+                )}
+            >
+                {props.title}
+            </h2>
             {typeof props.number === 'number' && (
                 <NumberCountup
                     number={props.number}
@@ -25,9 +39,26 @@ const Panel: FC<Props> = props => (
                 />
             )}
         </header>
-        {props.children && <div className={styles.chart}>{props.children}</div>}
+        {props.children && (
+            <div
+                className={twMerge(
+                    "[grid-area:'span 2 / 2'] self-end justify-self-end",
+                    'overflow-hidden font-mono text-xs font-medium',
+                    'text-(--textSecondary)'
+                )}
+            >
+                {props.children}
+            </div>
+        )}
         {props.footer && (
-            <footer className={styles.footer}>{props.footer}</footer>
+            <footer
+                className={twMerge(
+                    "[grid-area:'2 / 1'] self-end font-mono text-xs",
+                    'font-medium text-(--textSecondary)'
+                )}
+            >
+                {props.footer}
+            </footer>
         )}
     </section>
 )
