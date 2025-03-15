7c8a4562593139e7e16e559861bee27caa8db8c6
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-15 17:25:00 +0100

    Refactor search result

--- src/components/search/searchResult/searchResult.tsx
+++ src/components/search/searchResult/searchResult.tsx
@@ -10,40 +10,46 @@ export interface Props {
     heightOrHash: number | string
 }
 
-const SearchResult: FC<Props> = props => (
-    <li
-        className={clsx(
-            'transition-bg duration-200 ease-(--fastOutSlowIn)',
-            'not-last:border-b-1 not-last:border-b-(--surfaceLighter)',
-            'hover:bg-(--surface)'
-        )}
-    >
-        {typeof props.heightOrHash === 'number' ? (
-            <Link
-                className="flex gap-1 px-2 py-1.5 break-all text-(--text)!"
-                href={`/block/${props.heightOrHash}`}
-            >
-                <Box
-                    className="min-w-4"
-                    color="var(--textSecondary)"
-                    size={16}
-                />
-                {formatNumber(props.heightOrHash)}
-            </Link>
-        ) : (
-            <Link
-                className="flex gap-1 px-2 py-1.5 break-all text-(--text)!"
-                href={`/tx/${props.heightOrHash}`}
-            >
-                <CheckCheck
-                    className="min-w-4"
-                    color="var(--secondaryLight)"
-                    size={16}
-                />
-                {props.heightOrHash}
-            </Link>
-        )}
-    </li>
-)
+const SearchResult: FC<Props> = props => {
+    const linkClassName = clsx(
+        'flex gap-1 px-2 py-1.5 break-all text-(--text)!'
+    )
+
+    return (
+        <li
+            className={clsx(
+                'transition-bg duration-200 ease-(--fastOutSlowIn)',
+                'not-last:border-b-1 not-last:border-b-(--surfaceLighter)',
+                'hover:bg-(--surface)'
+            )}
+        >
+            {typeof props.heightOrHash === 'number' ? (
+                <Link
+                    className={linkClassName}
+                    href={`/block/${props.heightOrHash}`}
+                >
+                    <Box
+                        className="min-w-4"
+                        color="var(--textSecondary)"
+                        size={16}
+                    />
+                    {formatNumber(props.heightOrHash)}
+                </Link>
+            ) : (
+                <Link
+                    className={linkClassName}
+                    href={`/tx/${props.heightOrHash}`}
+                >
+                    <CheckCheck
+                        className="min-w-4"
+                        color="var(--secondaryLight)"
+                        size={16}
+                    />
+                    {props.heightOrHash}
+                </Link>
+            )}
+        </li>
+    )
+}
 
 export default SearchResult
