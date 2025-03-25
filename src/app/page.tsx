9a4e7b93748a5ae7c1e39441cde7c6faa50ae8fe
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-25 13:17:38 +0100

    Restore home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 // istanbul ignore file
 import { FC } from 'react'
+import {
+    BlockPanel,
+    BlockTable,
+    Button,
+    Container,
+    Search,
+    TransactionPanel,
+    TransactionTable,
+} from '@/components'
+import { getBlocks, getStats, getTransactions } from '@/lib/data'
+import GraphqlClientProvider from '@/lib/graphql/graphqlClientProvider'
 import { generatePageMetadata } from '@/lib/utils'
 
 export const metadata = generatePageMetadata(
@@ -11,52 +22,50 @@ export const metadata = generatePageMetadata(
 )
 
 const HomePage: FC = async () => {
-    return <div>Oops, something went wrong!</div>
+    const stats = await getStats()
+    const latestBlocks = await getBlocks({ latest: { limit: 10 } })
+    const latestTransactions = await getTransactions({ latest: { limit: 10 } })
 
-    // const stats = await getStats()
-    // const latestBlocks = await getBlocks({ latest: { limit: 10 } })
-    // const latestTransactions = await getTransactions({ latest: { limit: 10 } })
-    //
-    // let latestBlockHeight
-    //
-    // if (latestBlocks?.length) {
-    //     latestBlockHeight = latestBlocks[0].height
-    // }
-    //
-    // return (
-    //     <>
-    //         <Container>
-    //             <h1 className="font-secondary mb-2 text-4xl font-medium">
-    //                 Penumbra Blockchain Explorer
-    //             </h1>
-    //             <GraphqlClientProvider>
-    //                 <Search />
-    //             </GraphqlClientProvider>
-    //         </Container>
-    //         <Container className="grid grid-cols-6 gap-4">
-    //             <BlockPanel
-    //                 className="col-span-6 sm:col-span-3"
-    //                 number={latestBlockHeight}
-    //             />
-    //             <TransactionPanel
-    //                 className="col-span-6 sm:col-span-3"
-    //                 number={stats?.totalTransactionsCount}
-    //             />
-    //             <BlockTable
-    //                 actions={<Button href="/blocks">View all</Button>}
-    //                 blocks={latestBlocks}
-    //                 className="col-span-6 lg:col-span-3"
-    //                 title="Latest blocks"
-    //             />
-    //             <TransactionTable
-    //                 actions={<Button href="/txs">View all</Button>}
-    //                 className="col-span-6 lg:col-span-3"
-    //                 title="Latest transactions"
-    //                 transactions={latestTransactions}
-    //             />
-    //         </Container>
-    //     </>
-    // )
+    let latestBlockHeight
+
+    if (latestBlocks?.length) {
+        latestBlockHeight = latestBlocks[0].height
+    }
+
+    return (
+        <>
+            <Container>
+                <h1 className="font-secondary mb-2 text-4xl font-medium">
+                    Penumbra Blockchain Explorer
+                </h1>
+                <GraphqlClientProvider>
+                    <Search />
+                </GraphqlClientProvider>
+            </Container>
+            <Container className="grid grid-cols-6 gap-4">
+                <BlockPanel
+                    className="col-span-6 sm:col-span-3"
+                    number={latestBlockHeight}
+                />
+                <TransactionPanel
+                    className="col-span-6 sm:col-span-3"
+                    number={stats?.totalTransactionsCount}
+                />
+                <BlockTable
+                    actions={<Button href="/blocks">View all</Button>}
+                    blocks={latestBlocks}
+                    className="col-span-6 lg:col-span-3"
+                    title="Latest blocks"
+                />
+                <TransactionTable
+                    actions={<Button href="/txs">View all</Button>}
+                    className="col-span-6 lg:col-span-3"
+                    title="Latest transactions"
+                    transactions={latestTransactions}
+                />
+            </Container>
+        </>
+    )
 }
 
 export default HomePage
