1a4c265c580f19e60f39ca20e1210ac222cc4dd2
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-25 17:00:38 +0100

    Add search result overlay tests

---
+++ src/components/search/searchResultOverlay/searchResultOverlay.test.tsx
@@ -0,0 +1,20 @@
+import { getByText, render } from '@testing-library/react'
+import { SearchResult } from '../searchResult'
+import SearchResultOverlay from './searchResultOverlay'
+
+describe('SearchResultOverlay', () => {
+    test('renders title', async () => {
+        const { container } = render(<SearchResultOverlay title="Foo" />)
+        getByText(container, 'Foo')
+    })
+
+    test('renders children', async () => {
+        const { container } = render(
+            <SearchResultOverlay title="Foo">
+                <SearchResult heightOrHash={123} />
+            </SearchResultOverlay>
+        )
+
+        getByText(container, '123')
+    })
+})
