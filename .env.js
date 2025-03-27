8b0051d61fb9b2469cdd206cdf5eaf8b637ce8ba
Stéphane Chorne <stephane.chorne@gmail.com>
2025-03-27 14:50:02 +0100

    Refator .env.js

--- .env.js
+++ .env.js
@@ -10,7 +10,6 @@ const environments = {
         BASE_URL: 'https://explorer.penumbra.zone',
         NEXT_PUBLIC_FATHOM_ID: 'YHKFXWRT',
         NEXT_PUBLIC_GRAPHQL_URL: 'https://pe.dev.galaxid.xyz/',
-        NO_INDEX: false,
     },
     staging: {
         BASE_URL: 'https://explorer.penumbra.pklabs.me',
