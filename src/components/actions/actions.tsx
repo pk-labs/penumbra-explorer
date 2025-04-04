0d5f51454fb71e73472887f2591d0331f91cdd55
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-04 14:00:26 +0200

    Integrate react-error-boundary to catch action view errors

--- src/components/actions/actions.tsx
+++ src/components/actions/actions.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { ActionView } from '@penumbra-zone/ui/ActionView'
-import { FC, useMemo } from 'react'
+import { FC, useCallback, useMemo } from 'react'
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
 import { twMerge } from 'tailwind-merge'
 import { useGetMetadata } from '@/lib/hooks'
 import { decodeTransaction, transactionToView } from '@/lib/utils'
@@ -22,6 +23,20 @@ const Actions: FC<Props> = props => {
         return transactionToView(transaction, props.hash, props.blockHeight)
     }, [props.blockHeight, props.hash, props.rawTransaction])
 
+    const fallbackRender = useCallback(
+        ({ error }: FallbackProps) => (
+            <div
+                className={twMerge(
+                    'bg-other-tonalFill5 text-destructive-light flex h-10',
+                    'items-center rounded-sm px-3 py-2 text-sm'
+                )}
+            >
+                {String(error)}
+            </div>
+        ),
+        []
+    )
+
     return (
         <Subsection title="Actions">
             <ul className="flex flex-col gap-2">
@@ -34,7 +49,12 @@ const Actions: FC<Props> = props => {
                             'before:bg-other-tonalStroke last:before:hidden'
                         )}
                     >
-                        <ActionView action={action} getMetadata={getMetadata} />
+                        <ErrorBoundary fallbackRender={fallbackRender}>
+                            <ActionView
+                                action={action}
+                                getMetadata={getMetadata}
+                            />
+                        </ErrorBoundary>
                     </li>
                 ))}
             </ul>
