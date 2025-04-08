2d2b093e523c16749e17ed1ef29f063875323461
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-08 11:03:00 +0200

    Update GraphQL URL

--- .env.js
+++ .env.js
@@ -4,19 +4,19 @@ const environments = {
     dev: {
         BASE_URL: 'https://dev.explorer.penumbra.pklabs.me',
         NEXT_PUBLIC_GRAPHQL_URL:
-            'https://pe-backend-564694434950.europe-west6.run.app/graphql',
+            'http://api.explorer.penumbra.pklabs.me/graphql',
         NO_INDEX: true,
     },
     prod: {
         BASE_URL: 'https://explorer.penumbra.zone',
         NEXT_PUBLIC_FATHOM_ID: 'YHKFXWRT',
         NEXT_PUBLIC_GRAPHQL_URL:
-            'https://pe-backend-564694434950.europe-west6.run.app/graphql',
+            'http://api.explorer.penumbra.pklabs.me/graphql',
     },
     staging: {
         BASE_URL: 'https://explorer.penumbra.pklabs.me',
         NEXT_PUBLIC_GRAPHQL_URL:
-            'https://pe-backend-564694434950.europe-west6.run.app/graphql',
+            'http://api.explorer.penumbra.pklabs.me/graphql',
         NO_INDEX: true,
     },
 }
