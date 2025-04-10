d9c6fdf2fbd8dddc3e8cf4ed3eabcc4283cd1d89
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-10 20:18:33 +0200

    Revert previous change

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    headers: async () => [
-        {
-            headers: [
-                // Disable nginx buffering in Cloudflare to fix server events
-                {
-                    key: 'X-Accel-Buffering',
-                    value: 'no',
-                },
-            ],
-            source: '/:path*',
-        },
-    ],
     poweredByHeader: false,
     webpack: config => {
         config.module.rules.push({
