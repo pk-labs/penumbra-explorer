4836efd57a3030539bb6701ed00e2acebc98effc
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-10 23:17:57 +0200

    Switch to CoinGecko public API for UM price

--- src/lib/data/getUmPrice/getUmPrice.ts
+++ src/lib/data/getUmPrice/getUmPrice.ts
@@ -1,31 +1,32 @@
 import { UmPriceData } from '@/lib/types'
 
 const searchParams = new URLSearchParams({
-    baseAsset: 'UM',
-    durationWindow: '1d',
-    quoteAsset: 'USDC',
+    community_data: 'false',
+    developer_data: 'false',
+    localization: 'false',
+    sparkline: 'false',
+    tickers: 'false',
 })
 
-const url = `https://dex.penumbra.zone/api/summary?${searchParams.toString()}`
+const url = `https://api.coingecko.com/api/v3/coins/penumbra?${searchParams}`
 
 interface Data {
-    change: {
-        percent: string
-        sign: 'negative' | 'positive'
+    market_data: {
+        current_price: {
+            usd: number
+        }
+        price_change_percentage_24h: number
     }
-    price: string
 }
 
 const getUmPrice = async (): Promise<UmPriceData | undefined> => {
     try {
         const data: Data = await fetch(url).then(res => res.json())
+        console.log(data)
 
         return {
-            change:
-                data.change.sign === 'negative'
-                    ? -1 * Number(data.change.percent)
-                    : Number(data.change.percent),
-            price: Number(data.price),
+            change: data.market_data.price_change_percentage_24h,
+            price: data.market_data.current_price.usd,
         }
     } catch (e) {
         console.error(e)
