818719c2c777ddced6aa1fe365fa11be87e428b3
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-15 09:55:23 +0200

    Implement new data animation for block table

--- src/components/tables/blockTable/blockTable.tsx
+++ src/components/tables/blockTable/blockTable.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import { BoxIcon } from 'lucide-react'
 import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { TransformedPartialBlockFragment } from '@/lib/types'
 import { formatNumber } from '@/lib/utils'
 import { Table, TableCell, TableProps, TableRow } from '../table'
@@ -12,49 +14,66 @@ export interface Props
     proposer?: boolean
 }
 
-const BlockTable: FC<Props> = props => (
-    <Table
-        actions={props.actions}
-        className={props.className}
-        footer={props.footer}
-        title={props.title}
-    >
-        <thead>
-            <TableRow>
-                <TableCell header>Block height</TableCell>
-                <TableCell header>Time</TableCell>
-                {props.proposer && <TableCell header>Proposer</TableCell>}
-                <TableCell header>Txs</TableCell>
-            </TableRow>
-        </thead>
-        <tbody>
-            {props.blocks?.length ? (
-                props.blocks.map(block => (
-                    <TableRow
-                        key={block.height}
-                        href={`/block/${block.height}`}
-                    >
-                        <TableCell>
-                            <BoxIcon
-                                color="var(--color-text-secondary)"
-                                size={16}
-                            />
-                            <Link href={`/block/${block.height}`}>
-                                {formatNumber(block.height)}
-                            </Link>
-                        </TableCell>
-                        <TableCell>{block.timeAgo}</TableCell>
-                        {props.proposer && <TableCell>-</TableCell>}
-                        <TableCell>{block.transactionsCount}</TableCell>
-                    </TableRow>
-                ))
-            ) : (
+const BlockTable: FC<Props> = props => {
+    const prevBlocksRef = useRef<typeof props.blocks>(undefined)
+
+    useEffect(() => {
+        prevBlocksRef.current = props.blocks
+    }, [props.blocks])
+
+    return (
+        <Table
+            actions={props.actions}
+            className={props.className}
+            footer={props.footer}
+            title={props.title}
+        >
+            <thead>
                 <TableRow>
-                    <TableCell colSpan={props.proposer ? 4 : 3} />
+                    <TableCell header>Block height</TableCell>
+                    <TableCell header>Time</TableCell>
+                    {props.proposer && <TableCell header>Proposer</TableCell>}
+                    <TableCell header>Txs</TableCell>
                 </TableRow>
-            )}
-        </tbody>
-    </Table>
-)
+            </thead>
+            <tbody>
+                {props.blocks?.length ? (
+                    props.blocks.map(block => (
+                        <TableRow
+                            key={block.height}
+                            className={
+                                typeof prevBlocksRef.current !== 'undefined' &&
+                                !prevBlocksRef.current.some(
+                                    prevBlock =>
+                                        prevBlock.height === block.height
+                                )
+                                    ? 'animate-new-data-bg'
+                                    : undefined
+                            }
+                            href={`/block/${block.height}`}
+                        >
+                            <TableCell>
+                                <BoxIcon
+                                    color="var(--color-text-secondary)"
+                                    size={16}
+                                />
+                                <Link href={`/block/${block.height}`}>
+                                    {formatNumber(block.height)}
+                                </Link>
+                            </TableCell>
+                            <TableCell>{block.timeAgo}</TableCell>
+                            {props.proposer && <TableCell>-</TableCell>}
+                            <TableCell>{block.transactionsCount}</TableCell>
+                        </TableRow>
+                    ))
+                ) : (
+                    <TableRow>
+                        <TableCell colSpan={props.proposer ? 4 : 3} />
+                    </TableRow>
+                )}
+            </tbody>
+        </Table>
+    )
+}
 
 export default BlockTable
