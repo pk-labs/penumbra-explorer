7dea3d6b2d0f03f6d79de7a62661f12dd564dd8b
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-22 16:55:26 +0200

    Add chain page heading and basic info

--- src/app/ibc/[chainId]/page.tsx
+++ src/app/ibc/[chainId]/page.tsx
@@ -1,9 +1,10 @@
 // istanbul ignore file
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import { FC } from 'react'
 import { Breadcrumb, Breadcrumbs, Container } from '@/components'
 import { ibcConnections } from '@/lib/constants'
-import { generatePageMetadata } from '@/lib/utils'
+import { classNames, generatePageMetadata } from '@/lib/utils'
 
 interface Props {
     params: Promise<{ chainId: string }>
@@ -22,11 +23,11 @@ export const generateMetadata = async (props: Props) => {
 const ChainPage: FC<Props> = async props => {
     const params = await props.params
 
-    if (
-        !ibcConnections.some(
-            connection => connection.chainId === params.chainId
-        )
-    ) {
+    const connection = ibcConnections.find(
+        connection => connection.chainId === params.chainId
+    )
+
+    if (!connection) {
         notFound()
     }
 
@@ -36,6 +37,41 @@ const ChainPage: FC<Props> = async props => {
                 <Breadcrumb href="/">Explorer</Breadcrumb>
                 <Breadcrumb href="/ibc">IBC Chains</Breadcrumb>
             </Breadcrumbs>
+            <div
+                className={classNames(
+                    'bg-other-tonalFill5 flex flex-col gap-3 rounded-lg p-6',
+                    'backdrop-blur-[32px]'
+                )}
+            >
+                <h1
+                    className={classNames(
+                        'font-heading flex items-center gap-2 text-4xl',
+                        'font-medium'
+                    )}
+                >
+                    <Image
+                        alt={connection.name}
+                        height={32}
+                        src={connection.image}
+                        width={32}
+                    />
+                    {connection.name}
+                </h1>
+                <div
+                    className={classNames(
+                        'flex flex-col gap-1 font-mono text-sm font-medium'
+                    )}
+                >
+                    <div>
+                        <span className="text-text-secondary">chain-id </span>
+                        <span>{connection.chainId}</span>
+                    </div>
+                    <div>
+                        <span className="text-text-secondary">client-id </span>
+                        <span>???</span>
+                    </div>
+                </div>
+            </div>
         </Container>
     )
 }
