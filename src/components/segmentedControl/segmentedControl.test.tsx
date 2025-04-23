13db490367ad4a8404c9bf134f1f8cd5f64834ee
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-23 20:07:20 +0200

    Fix type error

--- src/components/segmentedControl/segmentedControl.test.tsx
+++ src/components/segmentedControl/segmentedControl.test.tsx
@@ -4,7 +4,13 @@ import SegmentedControl from './segmentedControl'
 describe('SegmentedControl', () => {
     test('applies CSS classes', async () => {
         const { container } = render(
-            <SegmentedControl className="foo bar">Foo</SegmentedControl>
+            <SegmentedControl
+                className="foo bar"
+                onChange={jest.fn()}
+                value="foo"
+            >
+                <SegmentedControl.Item value="foo" />
+            </SegmentedControl>
         )
 
         expect(container.firstChild).toHaveClass('foo', 'bar')
