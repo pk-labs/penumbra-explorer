4d5a904de5f1e59bfa11cd86acb283ed4d875591
Stéphane Chorne <stephane.chorne@gmail.com>
2025-04-23 20:17:48 +0200

    Truncate overflowing table cell text

--- src/components/tables/table/tableCell/tableCell.tsx
+++ src/components/tables/table/tableCell/tableCell.tsx
@@ -21,7 +21,7 @@ const TableCell: FC<Props> = props => {
     return (
         <Element
             className={classNames(
-                'h-12 px-3 text-left text-sm font-medium whitespace-nowrap',
+                'h-12 truncate px-3 text-left text-sm font-medium',
                 props.header
                     ? 'border-other-tonalFill10 text-text-secondary border-b ' +
                           'align-middle capitalize'
