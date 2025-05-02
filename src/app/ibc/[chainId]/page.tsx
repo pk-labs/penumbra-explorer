9dc7f607947f377a3264e8458c2167e4ff39ced7
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-02 14:38:15 +0200

    Filter IBC transactions by client ID

--- src/app/ibc/[chainId]/page.tsx
+++ src/app/ibc/[chainId]/page.tsx
@@ -58,7 +58,7 @@ const ChainPage: FC<Props> = async props => {
                 />
                 <PaginatedTransactionsContainer
                     className="min-w-0"
-                    // clientId={connection.clientId}
+                    clientId={connection.clientId}
                     header={
                         <h2
                             className={classNames(
