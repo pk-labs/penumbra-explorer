58ed290cbd69fc2cc805dbe2cbbf6718d1273cfe
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-02 22:11:42 +0200

    Remove no longer used transactionToJson

--- src/lib/utils/transactionToJson/transactionToJson.ts
+++
@@ -1,8 +0,0 @@
-// istanbul ignore file
-import { typeRegistry } from '@penumbra-zone/protobuf'
-import { Transaction } from '@penumbra-zone/protobuf/penumbra/core/transaction/v1/transaction_pb'
-
-const transactionToJson = (transaction: Transaction) =>
-    transaction.toJson({ typeRegistry }) as Record<string, any>
-
-export default transactionToJson
