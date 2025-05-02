f4bec7c7e511d4dcb5368abd146456e0acf9d659
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-02 22:04:38 +0200

    Refactor asset value

--- src/components/assetValue/assetValue.test.tsx
+++ src/components/assetValue/assetValue.test.tsx
@@ -1,19 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { render } from '@testing-library/react'
 import AssetValue from './assetValue'
 
-jest.mock('../../lib/hooks/useGetMetadata')
-
 describe('AssetValue', () => {
     test('applies CSS classes', async () => {
-        const { container } = render(
-            <AssetValue
-                // @ts-ignore
-                action={{ actionView: { case: 'delegate' } }}
-                className="foo bar"
-            />
-        )
-
+        const { container } = render(<AssetValue className="foo bar" />)
         expect(container.firstChild).toHaveClass('foo', 'bar')
     })
 })
--- src/components/assetValue/assetValue.tsx
+++ src/components/assetValue/assetValue.tsx
@@ -1,26 +1,19 @@
 'use client'
 
-import { Value } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb'
 import {
     ValueViewComponent,
     ValueViewComponentProps,
 } from '@penumbra-zone/ui/ValueView'
 import { FC } from 'react'
 
-interface Props extends Omit<ValueViewComponentProps<'default'>, 'valueView'> {
-    ammount: NonNullable<Value['amount']>
-    assetId: NonNullable<Value['assetId']>
+interface Props extends ValueViewComponentProps<'default'> {
     className?: string
 }
 
-const AssetValue: FC<Props> = ({ className, ...props }) => {
-    // const getMetadata = useGetMetadata(chainId)
-
-    return (
-        <div className={className}>
-            <ValueViewComponent {...props} />
-        </div>
-    )
-}
+const AssetValue: FC<Props> = ({ className, ...props }) => (
+    <div className={className}>
+        <ValueViewComponent {...props} />
+    </div>
+)
 
 export default AssetValue
