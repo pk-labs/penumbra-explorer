373691f5a4df4b75dc8d6fac2537b6f9e4f5106d
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-08 20:39:43 +0200

    Show ENV_NAME and APP_VERSION in footer

--- .env.js
+++ .env.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const packageJson = require('./package.json')
+
 const envName = process.env._ENV_NAME || 'dev'
 
 const environments = {
     dev: {
         BASE_URL: 'https://dev.explorer.penumbra.pklabs.me',
+        NEXT_PUBLIC_ENV_NAME: 'dev',
         NEXT_PUBLIC_GRAPHQL_HOST: 'dev.api.explorer.penumbra.pklabs.me',
         NO_INDEX: true,
     },
@@ -13,9 +17,13 @@ const environments = {
     },
     staging: {
         BASE_URL: 'https://explorer.penumbra.pklabs.me',
+        NEXT_PUBLIC_ENV_NAME: 'staging',
         NEXT_PUBLIC_GRAPHQL_HOST: 'api.explorer.penumbra.pklabs.me',
         NO_INDEX: true,
     },
 }
 
-module.exports = environments[envName]
+module.exports = {
+    ...environments[envName],
+    NEXT_PUBLIC_APP_VERSION: packageJson.version,
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
 // istanbul ignore file
 export const appName = 'Penumbra Blockchain Explorer'
+export const appVersion = process.env.NEXT_PUBLIC_APP_VERSION
+export const envName = process.env.NEXT_PUBLIC_ENV_NAME
 export const fastOutSlowIn = [0.4, 0, 0.2, 1]
