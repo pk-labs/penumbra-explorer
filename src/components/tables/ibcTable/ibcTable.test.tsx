c2052e2dd1f2693e05f06a942d9d85a25170dfbb
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-16 16:58:37 +0200

    Add IBC table tests

---
+++ src/components/tables/ibcTable/ibcTable.test.tsx
@@ -0,0 +1,14 @@
+import { getByText, render } from '@testing-library/react'
+import { TableProps } from '../table'
+import IbcTable from './ibcTable'
+
+jest.mock('../table/table', () => (props: TableProps) => (
+    <table>{props.children}</table>
+))
+
+describe('IbcTable', () => {
+    test('renders empty state when no clients', async () => {
+        const { container } = render(<IbcTable stats={[]} />)
+        getByText(container, 'No clients configured')
+    })
+})
