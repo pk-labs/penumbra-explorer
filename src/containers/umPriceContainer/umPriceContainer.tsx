c978755cc4a459fd6cc3ed32400743426bd1dd83
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-20 08:36:56 +0200

    Refactor UM price container to client component

--- src/containers/umPriceContainer/umPriceContainer.tsx
+++ src/containers/umPriceContainer/umPriceContainer.tsx
@@ -1,19 +1,30 @@
 // istanbul ignore file
-import { FC, Suspense } from 'react'
-import { Skeleton } from '@/components'
-import { classNames } from '@/lib/utils'
-import UmPriceLoader, { Props } from './umPriceLoader'
+'use client'
 
-const UmPriceContainer: FC<Props> = props => (
-    <Suspense
-        fallback={
-            <Skeleton
-                className={classNames('h-8 w-49 rounded-full', props.className)}
-            />
-        }
-    >
-        <UmPriceLoader {...props} />
-    </Suspense>
-)
+import { FC, useEffect, useState } from 'react'
+import { Skeleton, UmPrice } from '@/components'
+import { getUmPrice } from '@/lib/data'
+import { UmPriceData } from '@/lib/types'
+import { classNames } from '@/lib/utils'
+
+interface Props {
+    className?: string
+}
+
+const UmPriceContainer: FC<Props> = props => {
+    const [umPrice, setUmPrice] = useState<UmPriceData>()
+
+    useEffect(() => {
+        getUmPrice().then(setUmPrice)
+    }, [])
+
+    return umPrice ? (
+        <UmPrice className={props.className} {...umPrice} />
+    ) : (
+        <Skeleton
+            className={classNames('h-8 w-49 rounded-full', props.className)}
+        />
+    )
+}
 
 export default UmPriceContainer
