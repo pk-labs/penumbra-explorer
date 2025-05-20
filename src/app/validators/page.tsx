5818f5ab63f6634e5956386c336aa994dc5f1560
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-20 15:17:58 +0200

    Refactor validators page spacing

--- src/app/validators/page.tsx
+++ src/app/validators/page.tsx
@@ -20,7 +20,7 @@ const ValidatorsPage: FC = () => (
             <Breadcrumb href="/">Explore</Breadcrumb>
             <Breadcrumb>Validators</Breadcrumb>
         </Breadcrumbs>
-        <div className="grid grid-cols-6 gap-2">
+        <div className="grid grid-cols-6 gap-4">
             <StakedPanelContainer className="col-span-6 md:col-span-2" />
             <ValidatorsPanelContainer
                 className={classNames('col-span-6 sm:col-span-3 md:col-span-2')}
