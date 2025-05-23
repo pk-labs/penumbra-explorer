8d22f8340bca585297e55dcf92757bbafb177dc6
Stéphane Chorne <stephane.chorne@gmail.com>
2025-05-23 14:53:39 +0200

    Mock inactive validators

--- src/containers/validatorsPerformanceContainer/validatorsPerformanceLoader.tsx
+++ src/containers/validatorsPerformanceContainer/validatorsPerformanceLoader.tsx
@@ -14,8 +14,44 @@ export interface Props
 }
 
 const ValidatorsPerformanceLoader: FC<Props> = async ({ filter, ...props }) => {
-    const mockActiveValidators = () =>
-        Array.from({ length: 56 }).map(() => {
+    const mockActiveValidators = (count: number) =>
+        Array.from({ length: count }).map(() => {
+            const date = faker.date
+                .recent({
+                    days: faker.number.int({ max: 1000, min: 1 }),
+                })
+                .valueOf()
+
+            return {
+                bonding: faker.helpers.arrayElement([
+                    'Bonded',
+                    'Unbonding',
+                    'Unbonded',
+                ]),
+                commission: faker.number.int({ max: 10, min: 2 }),
+                hash: faker.finance.bitcoinAddress(),
+                initialTimeAgo: dayjs().to(date),
+                status: 'Active',
+                timestamp: date.valueOf(),
+                uptime: faker.datatype.boolean({ probability: 0.7 })
+                    ? 100
+                    : faker.number.float({
+                          max: 100,
+                          min: 95,
+                      }),
+                votingPower: faker.number.int({
+                    max: 999999,
+                    min: 100000,
+                }),
+                votingPowerPercentage: faker.number.float({
+                    max: 10,
+                    min: 1,
+                }),
+            }
+        })
+
+    const mockInactiveValidators = (count: number) =>
+        Array.from({ length: count }).map(() => {
             const date = faker.date
                 .recent({
                     days: faker.number.int({ max: 1000, min: 1 }),
@@ -32,7 +68,6 @@ const ValidatorsPerformanceLoader: FC<Props> = async ({ filter, ...props }) => {
                 hash: faker.finance.bitcoinAddress(),
                 initialTimeAgo: dayjs().to(date),
                 status: faker.helpers.arrayElement([
-                    'Active',
                     'Defined',
                     'Disabled',
                     'Inactive',
@@ -41,23 +76,16 @@ const ValidatorsPerformanceLoader: FC<Props> = async ({ filter, ...props }) => {
                     'Unspecified',
                 ]),
                 timestamp: date.valueOf(),
-                uptime: faker.datatype.boolean({ probability: 0.7 })
-                    ? 100
-                    : faker.datatype.boolean({ probability: 0.7 })
-                      ? faker.number.float({
-                            max: 100,
-                            min: 95,
-                        })
-                      : faker.number.float({
-                            max: 95,
-                            min: 0,
-                        }),
+                uptime: faker.number.float({
+                    max: 95,
+                    min: 0,
+                }),
                 votingPower: faker.number.int({
-                    max: 999999,
-                    min: 100000,
+                    max: 99999,
+                    min: 0,
                 }),
                 votingPowerPercentage: faker.number.float({
-                    max: 10,
+                    max: 1,
                     min: 0.1,
                 }),
             }
@@ -65,7 +93,12 @@ const ValidatorsPerformanceLoader: FC<Props> = async ({ filter, ...props }) => {
 
     const validators = await new Promise<any>(resolve =>
         setTimeout(
-            () => resolve(mockActiveValidators()),
+            () =>
+                resolve(
+                    filter === 'inactive'
+                        ? mockInactiveValidators(34)
+                        : mockActiveValidators(56)
+                ),
             faker.number.int({ max: 500, min: 300 })
         )
     )
