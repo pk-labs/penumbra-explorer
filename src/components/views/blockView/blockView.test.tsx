da43bd8c5569cc46fccb008a6837dd8856fb43de
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-10 12:50:09 +0200

    Add block view tests

--- src/components/views/blockView/blockView.test.tsx
+++ src/components/views/blockView/blockView.test.tsx
@@ -1,5 +1,10 @@
-import { queryByText, render } from '@testing-library/react'
+import { getByText, queryByText, render } from '@testing-library/react'
 import BlockView from './blockView'
+
+jest.mock(
+    '../../tables/transactionTable/transactionTable',
+    () => (props: any) => <div>{props.transactions}</div>
+)
 
 describe('BlockView', () => {
     test('renders navigation', async () => {
@@ -25,6 +30,37 @@ describe('BlockView', () => {
         expect(navigation?.firstChild).toHaveAttribute('href', '/block/2')
         expect(navigation?.lastChild).not.toHaveAttribute('href')
         expect(navigation?.lastChild?.firstChild).toBeDisabled()
+    })
+
+    test('renders block height', async () => {
+        const { container } = render(
+            <BlockView
+                block={{ height: 1234567, timestamp: 0, transactions: [] }}
+            />
+        )
+
+        getByText(container, '1,234,567')
+    })
+
+    test('renders timestamp height', async () => {
+        const { container } = render(
+            <BlockView
+                block={{ height: 1234567, timestamp: 0, transactions: [] }}
+            />
+        )
+
+        getByText(container, '1970-01-01 00:00:00 UTC')
+    })
+
+    test('renders transactions', async () => {
+        const { container } = render(
+            <BlockView
+                // @ts-expect-error
+                block={{ height: 1234567, timestamp: 0, transactions: 'Foo' }}
+            />
+        )
+
+        getByText(container, 'Foo')
     })
 
     test('applies CSS classes', async () => {
