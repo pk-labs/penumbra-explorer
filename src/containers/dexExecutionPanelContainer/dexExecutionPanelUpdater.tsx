8db910041985e2d47e45e8c3f816fc2ea2c2a997
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-14 12:57:57 +0200

    Mock dex panel real-time updates

--- src/containers/dexExecutionPanelContainer/dexExecutionPanelUpdater.tsx
+++ src/containers/dexExecutionPanelContainer/dexExecutionPanelUpdater.tsx
@@ -1,7 +1,8 @@
 // istanbul ignore file
 'use client'
 
-import { FC, useState } from 'react'
+import { faker } from '@faker-js/faker'
+import { FC, useEffect, useState } from 'react'
 import { NumberPanel } from '@/components'
 import { Props as DexExecutionPanelContainerProps } from './dexExecutionPanelContainer'
 
@@ -10,18 +11,29 @@ interface Props extends DexExecutionPanelContainerProps {
 }
 
 const DexExecutionPanelUpdater: FC<Props> = props => {
-    const [number] = useState(props.number)
+    const [number, setNumber] = useState(props.number)
 
-    // const [transactionCountUpdateSubscription] =
-    //     useTransactionCountUpdateSubscription()
-    //
-    // useEffect(() => {
-    //     if (transactionCountUpdateSubscription.data?.transactionCount) {
-    //         setNumber(
-    //             transactionCountUpdateSubscription.data.transactionCount.count
-    //         )
-    //     }
-    // }, [transactionCountUpdateSubscription.data?.transactionCount])
+    useEffect(() => {
+        let timeout: NodeJS.Timeout
+
+        const scheduleNextUpdate = () => {
+            timeout = setTimeout(
+                () => {
+                    setNumber(prev => prev + 1)
+                    scheduleNextUpdate()
+                },
+                faker.number.int({ max: 3000, min: 500 })
+            )
+        }
+
+        scheduleNextUpdate()
+
+        return () => {
+            if (timeout) {
+                clearTimeout(timeout)
+            }
+        }
+    }, [])
 
     return (
         <NumberPanel
--- src/containers/dexPositionPanelContainer/dexPositionPanelUpdater.tsx
+++ src/containers/dexPositionPanelContainer/dexPositionPanelUpdater.tsx
@@ -1,8 +1,9 @@
 // istanbul ignore file
 'use client'
 
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { NumberPanel } from '@/components'
+import { useTicker } from '@/lib/hooks'
 import { Props as DexPositionPanelContainerProps } from './dexPositionPanelContainer'
 
 interface Props extends DexPositionPanelContainerProps {
@@ -10,18 +11,34 @@ interface Props extends DexPositionPanelContainerProps {
 }
 
 const DexPositionPanelUpdater: FC<Props> = props => {
-    const [number] = useState(props.number)
+    const lastTick = useTicker()
+    const [number, setNumber] = useState(props.number)
 
-    // const [transactionCountUpdateSubscription] =
-    //     useTransactionCountUpdateSubscription()
-    //
-    // useEffect(() => {
-    //     if (transactionCountUpdateSubscription.data?.transactionCount) {
-    //         setNumber(
-    //             transactionCountUpdateSubscription.data.transactionCount.count
-    //         )
-    //     }
-    // }, [transactionCountUpdateSubscription.data?.transactionCount])
+    useEffect(() => {
+        if (lastTick.second() % 5 === 0) {
+            setNumber(prev => prev + 1)
+        }
+
+        // let timeout: NodeJS.Timeout
+        //
+        // const scheduleNextUpdate = () => {
+        //     timeout = setTimeout(
+        //         () => {
+        //             setNumber(prev => prev + 1)
+        //             scheduleNextUpdate()
+        //         },
+        //         faker.number.int({ max: 5000, min: 2000 })
+        //     )
+        // }
+        //
+        // scheduleNextUpdate()
+        //
+        // return () => {
+        //     if (timeout) {
+        //         clearTimeout(timeout)
+        //     }
+        // }
+    }, [lastTick])
 
     return (
         <NumberPanel
