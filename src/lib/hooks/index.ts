5bed9a5e459b0dcc569ef7cde5a875944c15d108
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-14 16:51:28 +0200

    Remove unused page visibility hook

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,5 +1,4 @@
 export { default as useDebounce } from './useDebounce'
 export { default as useGetMetadata } from './useGetMetadata'
 export { default as useLocalStorage } from './useLocalStorage'
-export { default as usePageVisibility } from './usePageVisibility'
 export { default as useTicker } from './useTicker'
