a1c28f4776df0ea8a75c36077c1f02d04771315e
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-14 17:52:30 +0200

    Improve validator status blocks animation

--- src/containers/validatorStatusContainer/validatorStatusBlocks.tsx
+++ src/containers/validatorStatusContainer/validatorStatusBlocks.tsx
@@ -1,34 +1,12 @@
 'use client'
 
-import { AnimatePresence, easeOut, motion } from 'motion/react'
+import { AnimatePresence, motion } from 'motion/react'
 import Link from 'next/link'
-import { FC, Fragment, MouseEvent, useCallback } from 'react'
+import { FC, MouseEvent, useCallback } from 'react'
 import { Tooltip } from '@/components'
 import { ValidatorBlock } from '@/lib/types'
 import { classNames, formatNumber } from '@/lib/utils'
 import styles from './validatorStatusContainer.module.css'
-
-const containerVariants = {
-    hidden: { opacity: 1 },
-    show: {
-        opacity: 1,
-        transition: {
-            staggerChildren: 0.0016,
-        },
-    },
-}
-
-const blockVariants = {
-    hidden: { scale: 0 },
-    show: {
-        scale: [0, 1.2, 1],
-        transition: {
-            duration: 0.2,
-            ease: easeOut,
-            times: [0, 0.7, 1],
-        },
-    },
-}
 
 interface Props {
     validatorBlocks: ValidatorBlock[]
@@ -38,45 +16,61 @@ const ValidatorStatusBlocks: FC<Props> = props => {
     const onContextMenu = useCallback((e: MouseEvent) => e.preventDefault(), [])
 
     return (
-        <motion.div
-            animate="show"
+        <div
             className="flex flex-wrap gap-2 select-none"
-            initial="hidden"
             onContextMenu={onContextMenu}
-            variants={containerVariants}
         >
-            <AnimatePresence>
-                {props.validatorBlocks.map(block => (
-                    <Fragment key={block.height}>
-                        <Link
-                            className={styles.link}
-                            href={`/block/${block.height}`}
-                            id={`block-${block.height}`}
-                        >
-                            <motion.span
-                                className={classNames(
-                                    styles.block,
-                                    block.signed === true && styles.signed,
-                                    block.signed === false && styles.missed
-                                )}
-                                exit="exit"
-                                variants={blockVariants}
-                                layout
-                            />
-                        </Link>
-                        <Tooltip
-                            anchorSelect={`#block-${block.height}`}
-                            className="flex flex-col items-center"
-                        >
-                            Block height
-                            <span className="text-sm">
-                                {formatNumber(block.height)}
-                            </span>
-                        </Tooltip>
-                    </Fragment>
+            <AnimatePresence mode="popLayout">
+                {props.validatorBlocks.map((block, i) => (
+                    <Link
+                        key={block.height}
+                        className={styles.link}
+                        href={`/block/${block.height}`}
+                        id={`block-${block.height}`}
+                    >
+                        <motion.span
+                            animate={{
+                                opacity: 1,
+                                scale: 1,
+                                transition: { delay: 0.2 + i * 0.0016 },
+                            }}
+                            className={classNames(
+                                styles.block,
+                                block.signed === true && styles.signed,
+                                block.signed === false && styles.missed
+                            )}
+                            exit={{
+                                opacity: 0,
+                                scale: 0.8,
+                            }}
+                            initial={{
+                                opacity: 0,
+                                scale: 0.8,
+                            }}
+                            transition={{
+                                duration: 0.3,
+                                layout: {
+                                    duration: 0.5,
+                                },
+                            }}
+                            layout
+                        />
+                    </Link>
                 ))}
             </AnimatePresence>
-        </motion.div>
+            {props.validatorBlocks.map(block => (
+                <Tooltip
+                    key={block.height}
+                    anchorSelect={`#block-${block.height}`}
+                    className="flex flex-col items-center"
+                >
+                    Block height
+                    <span className="text-sm">
+                        {formatNumber(block.height)}
+                    </span>
+                </Tooltip>
+            ))}
+        </div>
     )
 }
 
