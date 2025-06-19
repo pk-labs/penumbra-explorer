fb3189c04daafb94fc65feddb3991327b9120e62
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-19 14:15:18 +0200

    Refactor dex execution route

--- src/components/dexExecution/dexExecutionRoute.tsx
+++ src/components/dexExecution/dexExecutionRoute.tsx
@@ -7,6 +7,51 @@ interface Props {
 }
 
 const DexExecutionRoute: FC<Props> = props => {
+    const separator = (
+        <span className="border-t-other-tonalStroke min-w-4 flex-1 border-t-1" />
+    )
+
+    if (props.hops.length > 3) {
+        const firstHop = props.hops[0]
+        const collapsedHops = props.hops.length - 2
+        const lastHop = props.hops[props.hops.length - 1]
+
+        return (
+            <div className="flex items-center">
+                <span className="flex flex-1 items-center">
+                    <span
+                        className={classNames(
+                            'bg-other-tonalFill10 rounded-full px-2 py-1',
+                            'whitespace-nowrap'
+                        )}
+                    >
+                        {formatNumber(firstHop.amount)} {firstHop.currency}
+                    </span>
+                    {separator}
+                </span>
+                <span
+                    className={classNames(
+                        'bg-other-tonalFill10 rounded-full px-2 py-1',
+                        'whitespace-nowrap'
+                    )}
+                >
+                    +{collapsedHops}
+                </span>
+                <span className="flex flex-1 items-center">
+                    {separator}
+                    <span
+                        className={classNames(
+                            'bg-other-tonalFill10 rounded-full px-2 py-1',
+                            'whitespace-nowrap'
+                        )}
+                    >
+                        {formatNumber(lastHop.amount)} {lastHop.currency}
+                    </span>
+                </span>
+            </div>
+        )
+    }
+
     const lastIndex = props.hops.length - 1
 
     return (
@@ -21,14 +66,7 @@ const DexExecutionRoute: FC<Props> = props => {
                     >
                         {formatNumber(hop.amount)} {hop.currency}
                     </span>
-                    {i !== lastIndex && (
-                        <span
-                            className={classNames(
-                                'border-t-other-tonalStroke min-w-8 flex-1',
-                                'border-t-1'
-                            )}
-                        />
-                    )}
+                    {i !== lastIndex && separator}
                 </Fragment>
             ))}
         </div>
