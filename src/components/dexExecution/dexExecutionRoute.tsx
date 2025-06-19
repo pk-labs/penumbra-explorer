e7d019a6c83994881de7752bb06c600ef92f8b59
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-19 14:33:21 +0200

    Refactor dex execution route

--- src/components/dexExecution/dexExecutionRoute.tsx
+++ src/components/dexExecution/dexExecutionRoute.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from 'react'
+import { FC } from 'react'
 import { DexExecutionHop } from '@/lib/types'
 import { classNames, formatNumber } from '@/lib/utils'
 
@@ -7,14 +7,15 @@ interface Props {
 }
 
 const DexExecutionRoute: FC<Props> = props => {
+    const firstHop = props.hops[0]
+    const lastHop = props.hops[props.hops.length - 1]
+
     const separator = (
         <span className="border-t-other-tonalStroke min-w-4 flex-1 border-t-1" />
     )
 
-    if (props.hops.length > 3) {
-        const firstHop = props.hops[0]
+    if (props.hops.length > 2) {
         const collapsedHops = props.hops.length - 2
-        const lastHop = props.hops[props.hops.length - 1]
 
         return (
             <div className="flex items-center">
@@ -52,23 +53,25 @@ const DexExecutionRoute: FC<Props> = props => {
         )
     }
 
-    const lastIndex = props.hops.length - 1
-
     return (
         <div className="flex items-center">
-            {props.hops.map((hop, i) => (
-                <Fragment key={i}>
-                    <span
-                        className={classNames(
-                            'bg-other-tonalFill10 rounded-full px-2 py-1',
-                            'whitespace-nowrap'
-                        )}
-                    >
-                        {formatNumber(hop.amount)} {hop.currency}
-                    </span>
-                    {i !== lastIndex && separator}
-                </Fragment>
-            ))}
+            <span
+                className={classNames(
+                    'bg-other-tonalFill10 rounded-full px-2 py-1',
+                    'whitespace-nowrap'
+                )}
+            >
+                {formatNumber(firstHop.amount)} {firstHop.currency}
+            </span>
+            {separator}
+            <span
+                className={classNames(
+                    'bg-other-tonalFill10 rounded-full px-2 py-1',
+                    'whitespace-nowrap'
+                )}
+            >
+                {formatNumber(lastHop.amount)} {lastHop.currency}
+            </span>
         </div>
     )
 }
