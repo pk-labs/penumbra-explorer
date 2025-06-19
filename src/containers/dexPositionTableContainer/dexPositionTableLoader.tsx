8ba6e3cbdeced21af492a2a9cd74327301c43c76
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-19 15:00:03 +0200

    Refactor dex position table container

--- src/containers/dexPositionTableContainer/dexPositionTableLoader.tsx
+++ src/containers/dexPositionTableContainer/dexPositionTableLoader.tsx
@@ -1,18 +1,9 @@
 // istanbul ignore file
 import { faker } from '@faker-js/faker'
-import { ExternalLinkIcon } from 'lucide-react'
 import { FC } from 'react'
-import {
-    DexPositionStatePill,
-    EmptyState,
-    Table,
-    TableCell,
-    TableRow,
-    TimeAgo,
-} from '@/components'
 import dayjs from '@/lib/dayjs'
 import { DexPositionState, TransformedDexPosition } from '@/lib/types'
-import { classNames, formatNumber, shortenHash } from '@/lib/utils'
+import DexPositionTable from '../../components/tables/dexPositionTable/dexPositionTable'
 import { Props } from './dexPositionTableContainer'
 
 const currencies = ['ATOM', 'CDT', 'OSMO', 'TIA', 'UM', 'USDC']
@@ -57,62 +48,11 @@ const DexPositionTableLoader: FC<Props> = async props => {
     )
 
     return (
-        <Table className={props.className} header={props.header}>
-            <thead>
-                <TableRow>
-                    <TableCell header>Pair</TableCell>
-                    <TableCell header>Reserves</TableCell>
-                    <TableCell header>State</TableCell>
-                    <TableCell header>Fee tier</TableCell>
-                    <TableCell header>Updated</TableCell>
-                    <TableCell header>Position ID</TableCell>
-                </TableRow>
-            </thead>
-            <tbody>
-                {positions?.length ? (
-                    positions.map(position => (
-                        <TableRow key={position.id}>
-                            <TableCell className="h-20">
-                                {position.base}/{position.quote}
-                            </TableCell>
-                            <TableCell className="h-20">
-                                {formatNumber(position.reserve)}{' '}
-                                {position.quote}
-                            </TableCell>
-                            <TableCell className="h-20">
-                                <DexPositionStatePill state={position.state} />
-                            </TableCell>
-                            <TableCell className="h-20">
-                                {position.fee.toFixed(2)}%
-                            </TableCell>
-                            <TableCell className="h-20">
-                                <TimeAgo timestamp={position.timestamp} />
-                            </TableCell>
-                            <TableCell className="h-20">
-                                <a
-                                    className={classNames(
-                                        'hover:text-text-special inline-flex',
-                                        'items-center gap-1'
-                                    )}
-                                    href={`https://dex.penumbra.zone/inspect/lp/${position.id}`}
-                                    rel="nofollow"
-                                    target="_blank"
-                                >
-                                    {shortenHash(position.id, 9, 'end')}
-                                    <ExternalLinkIcon size={16} />
-                                </a>
-                            </TableCell>
-                        </TableRow>
-                    ))
-                ) : (
-                    <TableRow>
-                        <TableCell className="h-20" colSpan={6}>
-                            <EmptyState>No positions found</EmptyState>
-                        </TableCell>
-                    </TableRow>
-                )}
-            </tbody>
-        </Table>
+        <DexPositionTable
+            className={props.className}
+            header={props.header}
+            positions={positions}
+        />
     )
 }
 
