683a51531587bfaf78216bb9cd6704efe4d6352f
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-26 13:47:10 +0200

    Show all dex execution route hops

--- src/components/dexSwapExecution/dexExecutionRoute.tsx
+++ src/components/dexSwapExecution/dexExecutionRoute.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, Fragment } from 'react'
 import { DexExecutionHop } from '@/lib/types'
 import { classNames } from '@/lib/utils'
 import AssetValue from '../assetValue'
@@ -7,46 +7,22 @@ interface Props {
     hops: DexExecutionHop[]
 }
 
-const DexExecutionRoute: FC<Props> = props => {
-    const firstHop = props.hops[0]
-    const lastHop = props.hops[props.hops.length - 1]
-
-    const separator = (
-        <span className="border-t-other-tonalStroke min-w-4 flex-1 border-t-1" />
-    )
-
-    if (props.hops.length > 2) {
-        const collapsedHops = props.hops.length - 2
-
-        return (
-            <div className="flex items-center">
-                <span className="flex flex-1 items-center">
-                    <AssetValue {...firstHop} density="slim" />
-                    {separator}
+const DexExecutionRoute: FC<Props> = props => (
+    <div className="flex items-center">
+        {props.hops.map((hop, i) => (
+            <Fragment key={i}>
+                <span key={i} className="flex items-center">
+                    <AssetValue {...hop} density="slim" />
                 </span>
                 <span
                     className={classNames(
-                        'bg-other-tonalFill10 rounded-full px-2 py-1',
-                        'whitespace-nowrap'
+                        'border-t-other-tonalStroke min-w-4 flex-1 border-t-1',
+                        'last:hidden'
                     )}
-                >
-                    +{collapsedHops}
-                </span>
-                <span className="flex flex-1 items-center">
-                    {separator}
-                    <AssetValue {...lastHop} density="slim" />
-                </span>
-            </div>
-        )
-    }
-
-    return (
-        <div className="flex items-center">
-            <AssetValue {...firstHop} density="slim" />
-            {separator}
-            <AssetValue {...lastHop} density="slim" />
-        </div>
-    )
-}
+                />
+            </Fragment>
+        ))}
+    </div>
+)
 
 export default DexExecutionRoute
