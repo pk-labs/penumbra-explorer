7b7e1b76fa9047ded14b5dcef0dbc9a79d4efecf
Stéphane Chorne <stephane.chorne@gmail.com>
2025-06-27 18:24:06 +0200

    Refactor asset value tests

--- src/components/assetValue/assetValue.test.tsx
+++ src/components/assetValue/assetValue.test.tsx
@@ -1,5 +1,6 @@
 import { getByText, render } from '@testing-library/react'
 import { useAsset } from '@/lib/hooks'
+import AssetValue from './assetValue'
 
 jest.mock('../../lib/hooks/useAsset/useAsset')
 jest.mock('../skeleton/skeleton', () => () => <div>Skeleton</div>)
@@ -10,7 +11,6 @@ describe('AssetValue', () => {
     test('renders skeleton when registry is loading', async () => {
         mockedUseAsset.mockImplementationOnce(() => undefined)
 
-        const { default: AssetValue } = await import('./assetValue')
         const { container } = render(<AssetValue amount={0} assetId="foo" />)
 
         getByText(container, 'Skeleton')
@@ -19,7 +19,6 @@ describe('AssetValue', () => {
     test('renders NA when an asset is null', async () => {
         mockedUseAsset.mockImplementationOnce(() => null)
 
-        const { default: AssetValue } = await import('./assetValue')
         const { container } = render(<AssetValue amount={0} assetId="foo" />)
 
         getByText(container, 'NA')
@@ -28,8 +27,6 @@ describe('AssetValue', () => {
     test('applies CSS classes', async () => {
         // @ts-expect-error
         mockedUseAsset.mockImplementationOnce((id: string) => ({ symbol: id }))
-
-        const { default: AssetValue } = await import('./assetValue')
 
         const { container } = render(
             <AssetValue amount={0} assetId="foo" className="foo bar" />
