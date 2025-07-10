cf1eed128f2ae85f769dff7597755b12d01c6e91
Stéphane Chorne <stephane.chorne@gmail.com>
2025-07-10 14:58:47 +0200

    Refactor home page layout and fix spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
     TransactionTableContainer,
 } from '@/containers'
 import GraphqlClientProvider from '@/lib/graphql/graphqlClientProvider'
-import { classNames, generatePageMetadata } from '@/lib/utils'
+import { generatePageMetadata } from '@/lib/utils'
 
 export const metadata = generatePageMetadata(
     'Noctis',
@@ -19,7 +19,6 @@ export const metadata = generatePageMetadata(
     '/'
 )
 
-// FIXME: Extra spacing between panels and tables above md and below lg
 const HomePage: FC = () => (
     <>
         <Container>
@@ -30,58 +29,44 @@ const HomePage: FC = () => (
                 <SearchContainer />
             </GraphqlClientProvider>
         </Container>
-        <Container className="grid grid-cols-6 gap-4">
-            <BlockPanelContainer
-                className={classNames(
-                    'col-span-6 col-start-1 row-1 md:col-span-3 md:col-start-1'
-                )}
-            />
-            <BlockTableContainer
-                className={classNames(
-                    'col-span-6 col-start-1 row-4 lg:col-span-3 lg:row-2'
-                )}
-                header={
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-2xl font-medium">Latest blocks</h2>
-                        <Button density="compact" href="/blocks">
-                            View all
-                        </Button>
-                    </div>
-                }
-                limit={{ length: 10 }}
-                subscription
-            />
-            <TransactionPanelContainer
-                className={classNames(
-                    'col-span-6 col-start-1 row-2 md:col-span-3',
-                    'md:col-start-4 md:row-1'
-                )}
-            />
-            {/*<ShieldedPanelContainer*/}
-            {/*    className={classNames(*/}
-            {/*        'col-span-6 col-start-1 row-3 lg:col-span-2',*/}
-            {/*        'lg:col-start-5 lg:row-1'*/}
-            {/*    )}*/}
-            {/*/>*/}
-            <TransactionTableContainer
-                className={classNames(
-                    'col-span-6 col-start-1 row-5 lg:col-span-3',
-                    'lg:col-start-4 lg:row-2'
-                )}
-                header={
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-2xl font-medium">
-                            Latest transactions
-                        </h2>
-                        <Button density="compact" href="/txs">
-                            View all
-                        </Button>
-                    </div>
-                }
-                limit={{ length: 10 }}
-                blockHeight
-                subscription
-            />
+        <Container className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 md:flex-row">
+                <BlockPanelContainer className="flex-1" />
+                <TransactionPanelContainer className="flex-1" />
+            </div>
+            <div className="flex flex-col gap-4 lg:flex-row">
+                <BlockTableContainer
+                    className="flex-1"
+                    header={
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-2xl font-medium">
+                                Latest blocks
+                            </h2>
+                            <Button density="compact" href="/blocks">
+                                View all
+                            </Button>
+                        </div>
+                    }
+                    limit={{ length: 10 }}
+                    subscription
+                />
+                <TransactionTableContainer
+                    className="flex-1"
+                    header={
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-2xl font-medium">
+                                Latest transactions
+                            </h2>
+                            <Button density="compact" href="/txs">
+                                View all
+                            </Button>
+                        </div>
+                    }
+                    limit={{ length: 10 }}
+                    blockHeight
+                    subscription
+                />
+            </div>
         </Container>
     </>
 )
