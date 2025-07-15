9263cf24ca089e3f0d6c86f2a2a0701eb26a09c4
Stéphane Chorne <stephane.chorne@gmail.com>
2025-07-15 13:40:11 +0200

    Disable proposal panel container skeleton for now

--- src/containers/proposalPanelContainer/proposalPanelContainer.tsx
+++ src/containers/proposalPanelContainer/proposalPanelContainer.tsx
@@ -1,7 +1,5 @@
 // istanbul ignore file
 import { FC, Suspense } from 'react'
-import { Skeleton } from '@/components'
-import { classNames } from '@/lib/utils'
 import ProposalPanelLoader from './proposalPanelLoader'
 
 export interface Props {
@@ -10,19 +8,20 @@ export interface Props {
 
 const ProposalPanelContainer: FC<Props> = props => (
     <Suspense
-        fallback={
-            <div
-                className={classNames(
-                    'border-other-tonalFill10 rounded-lg border-1',
-                    'bg-linear-284 from-[rgba(186,77,20,0.05)] from-[9.77%]',
-                    'to-[rgba(193,166,204,0.35)] to-[99.84%] px-6 py-4',
-                    'backdrop-blur-lg',
-                    props.className
-                )}
-            >
-                <Skeleton className="h-18.5 rounded-sm sm:h-11" />
-            </div>
-        }
+    // No fallback for now because active proposals are infrequent
+    // fallback={
+    //     <div
+    //         className={classNames(
+    //             'border-other-tonalFill10 rounded-lg border-1',
+    //             'bg-linear-284 from-[rgba(186,77,20,0.05)] from-[9.77%]',
+    //             'to-[rgba(193,166,204,0.35)] to-[99.84%] px-6 py-4',
+    //             'backdrop-blur-lg',
+    //             props.className
+    //         )}
+    //     >
+    //         <Skeleton className="h-18.5 rounded-sm sm:h-11" />
+    //     </div>
+    // }
     >
         <ProposalPanelLoader {...props} />
     </Suspense>
