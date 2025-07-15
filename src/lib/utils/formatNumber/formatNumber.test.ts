332bbd2a57db9f68fbe656028c594ce6099d2da7
Stéphane Chorne <stephane.chorne@gmail.com>
2025-07-15 17:16:06 +0200

    Fix number formatting with fixed decimal places

--- src/lib/utils/formatNumber/formatNumber.test.ts
+++ src/lib/utils/formatNumber/formatNumber.test.ts
@@ -1,7 +1,12 @@
 import formatNumber from './formatNumber'
 
 describe('formatNumber', () => {
-    test('formats number as expected', async () => {
-        expect(formatNumber('1234567.89')).toBe('1,234,567.89')
+    test('formats number as is', async () => {
+        expect(formatNumber(1234567.89)).toBe('1,234,567.89')
+        expect(formatNumber(100)).toBe('100')
+    })
+
+    test('formats number to fixed decimal places', async () => {
+        expect(formatNumber(100, 2)).toBe('100.00')
     })
 })
