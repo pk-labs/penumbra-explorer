a2b628e8ddfb60e4cdd43ed7b2a5ff9e0322ef35
Stéphane Chorne <stephane.chorne@gmail.com>
2025-07-24 20:34:20 +0200

    Refactor block panel animation

--- src/components/panels/blockPanel/blockPanel.test.tsx
+++ src/components/panels/blockPanel/blockPanel.test.tsx
@@ -72,33 +72,17 @@ describe('BlockPanel', () => {
         })
     })
 
-    describe('does not animate cube', () => {
-        test('when no block', async () => {
-            const { container } = render(<BlockPanel />)
-            expect(container.querySelector('.cube')).not.toHaveClass('animated')
-        })
+    test('cube does not rotate when no block', async () => {
+        const { container } = render(<BlockPanel />)
 
-        test('when not syncing', async () => {
-            const { container, rerender } = render(
-                <BlockPanel blockHeight={99} />
-            )
+        expect(container.querySelector('.cube')).not.toHaveClass(
+            'rotateInfinite'
+        )
+    })
 
-            rerender(<BlockPanel blockHeight={100} />)
-
-            act(() => jest.advanceTimersByTime(6000))
-            getByText(container, 'Next block in ~0s')
-
-            act(() => jest.advanceTimersByTime(1000))
-            getByText(container, 'Next block late by ~1s')
-
-            act(() => jest.advanceTimersByTime(29000))
-            getByText(container, 'Next block late by ~30s')
-
-            act(() => jest.advanceTimersByTime(1000))
-            getByText(container, 'Blocks not synced')
-
-            expect(container.querySelector('.cube')).not.toHaveClass('animated')
-        })
+    test('cube rotates when syncing', async () => {
+        const { container } = render(<BlockPanel blockHeight={123} />)
+        expect(container.querySelector('.cube')).toHaveClass('rotateInfinite')
     })
 
     test('applies CSS classes', async () => {
--- src/components/panels/blockPanel/blockPanelChart.tsx
+++ src/components/panels/blockPanel/blockPanelChart.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { FC, useCallback, useEffect, useRef, useState } from 'react'
+import { classNames } from '@/lib/utils'
 import styles from './blockPanel.module.css'
-import BlockPanelMessage from './blockPanelMessage'
 
 const barCount = 25
 const upcomingCountdown = 5
@@ -21,25 +21,38 @@ interface Props {
 }
 
 const BlockPanelChart: FC<Props> = props => {
-    const chartRef = useRef<HTMLDivElement>(null)
+    const barsRef = useRef<HTMLDivElement>(null)
+    const cubeRef = useRef<HTMLDivElement>(null)
     const initialBlock = useRef(true)
     const [blockHeight, setBlockHeight] = useState(props.blockHeight)
     const [syncState, setSyncState] = useState(SyncState.Syncing)
     const [counter, setCounter] = useState<number>()
 
     const resetBars = useCallback(() => {
-        if (chartRef.current) {
-            const bars = Array.from(chartRef.current.children)
-            bars.forEach(bar => bar.classList.remove(styles.full))
+        if (barsRef.current) {
+            const bars = Array.from(barsRef.current.children)
+            bars.forEach(bar => bar.classList.remove(styles.fullBar))
+        }
+    }, [])
+
+    const resetCube = useCallback(() => {
+        if (cubeRef.current) {
+            cubeRef.current.classList.remove(
+                styles.rotateInfinite,
+                styles.rotateFirstHalf,
+                styles.rotateSecondHalf
+            )
         }
     }, [])
 
     useEffect(() => {
-        if (!chartRef.current || !props.blockHeight) {
+        if (!barsRef.current || !cubeRef.current || !props.blockHeight) {
             return
         }
 
         if (syncState === SyncState.Syncing && blockHeight) {
+            cubeRef.current.classList.add(styles.rotateInfinite)
+
             if (props.blockHeight - blockHeight >= syncMinBlockHeight) {
                 return
             } else if (initialBlock.current) {
@@ -50,19 +63,26 @@ const BlockPanelChart: FC<Props> = props => {
             }
         } else if (props.blockHeight !== blockHeight) {
             resetBars()
+            resetCube()
             setBlockHeight(props.blockHeight)
             setCounter(upcomingCountdown)
             setSyncState(SyncState.Upcoming)
         }
-    }, [props.blockHeight, blockHeight, syncState, resetBars])
+    }, [props.blockHeight, blockHeight, syncState, resetBars, resetCube])
 
     useEffect(() => {
-        if (!chartRef.current || syncState !== SyncState.Upcoming) {
+        if (
+            !barsRef.current ||
+            !cubeRef.current ||
+            syncState !== SyncState.Upcoming
+        ) {
             return
         }
 
-        const bars = Array.from(chartRef.current.children)
+        const bars = Array.from(barsRef.current.children)
         const barsPerSecond = barCount / upcomingCountdown
+
+        const cube = cubeRef.current
 
         const interval = setInterval(() => {
             if (initialBlock.current) {
@@ -71,21 +91,33 @@ const BlockPanelChart: FC<Props> = props => {
 
             setCounter(prev => prev && prev - 1)
 
-            bars.filter(bar => !bar.classList.contains(styles.full))
+            bars.filter(bar => !bar.classList.contains(styles.fullBar))
                 .slice(0, barsPerSecond)
                 .forEach((bar, i) => {
-                    bar.classList.add(styles.full)
+                    bar.classList.add(styles.fullBar)
 
                     if (i) {
-                        bar.classList.add(styles[`delayed-${i}`])
+                        bar.classList.add(styles[`delayed${i}`])
                     }
                 })
+
+            cube.classList.remove(styles.rotateInfinite)
+
+            if (cube.classList.contains(styles.rotateSecondHalf)) {
+                cube.classList.remove(styles.rotateSecondHalf)
+                cube.classList.add(styles.rotateFirstHalf)
+            } else if (cube.classList.contains(styles.rotateFirstHalf)) {
+                cube.classList.remove(styles.rotatrotateFirstHalfeSecondHalf)
+                cube.classList.add(styles.rotateSecondHalf)
+            } else {
+                cube.classList.add(styles.rotateFirstHalf)
+            }
         }, 1000)
 
         return () => {
             clearInterval(interval)
         }
-    }, [syncState, props])
+    }, [syncState, props, resetCube])
 
     useEffect(() => {
         if (syncState !== SyncState.Upcoming || counter !== 0) {
@@ -115,7 +147,7 @@ const BlockPanelChart: FC<Props> = props => {
         return () => {
             clearTimeout(interval)
         }
-    }, [resetBars, syncState])
+    }, [syncState])
 
     useEffect(() => {
         if (syncState === SyncState.Late && counter && counter > syncTimeout) {
@@ -124,21 +156,68 @@ const BlockPanelChart: FC<Props> = props => {
         }
     }, [counter, resetBars, syncState])
 
+    let message
+    let color
+
+    switch (syncState) {
+        case SyncState.Upcoming:
+            message = `Next block in ~${counter}s`
+            color = 'text-primary'
+            break
+        case SyncState.Late:
+            message = `Next block late by ~${counter}s`
+            color = 'text-secondary'
+            break
+        case SyncState.NotSynced:
+            message = 'Blocks not synced'
+            color = 'caution-light'
+            break
+        default:
+            message = 'Syncing to blocks ...'
+            color = 'text-secondary'
+            break
+    }
+
     return (
         <div className="flex flex-col gap-4 sm:gap-2 sm:self-end">
             <div
-                ref={chartRef}
+                ref={barsRef}
                 className="flex justify-between sm:justify-start sm:gap-1"
             >
                 {Array.from({ length: barCount }).map((_, i) => (
-                    <div key={i} className={styles.bar} />
+                    <div
+                        key={i}
+                        className={classNames(
+                            'bg-other-tonal-fill10 h-2.5 w-1.25 rounded-xs',
+                            'sm:h-9 sm:w-1 xl:h-10! xl:w-1.25!'
+                        )}
+                    />
                 ))}
             </div>
-            <BlockPanelMessage
-                blockHeight={blockHeight}
-                seconds={counter}
-                syncState={syncState}
-            />
+            <div className="flex items-center justify-end gap-1.5">
+                <div
+                    ref={cubeRef}
+                    className={classNames(
+                        'relative h-2.5 w-2.5 scale-75 transform-3d',
+                        'sm:scale-80 xl:scale-85!',
+                        styles.cube
+                    )}
+                >
+                    {Array.from({ length: 6 }).map((_, i) => (
+                        <div
+                            key={i}
+                            className={classNames(
+                                `absolute h-full w-full border-${color}`,
+                                'border-1 bg-neutral-900',
+                                styles.face
+                            )}
+                        />
+                    ))}
+                </div>
+                <div className={`text-${color} self-end font-mono text-xs`}>
+                    {message}
+                </div>
+            </div>
         </div>
     )
 }
