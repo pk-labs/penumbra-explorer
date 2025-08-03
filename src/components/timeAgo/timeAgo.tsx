946afcd3d5473199b7973575d4913ef2095082a4
Stéphane Chorne <stephane.chorne@gmail.com>
2025-08-03 18:05:30 +0200

    Revert time ago change due to hydration mismatch

--- src/components/timeAgo/timeAgo.tsx
+++ src/components/timeAgo/timeAgo.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC, Suspense, useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useTicker } from '@/lib/hooks'
 
 interface Props {
@@ -10,13 +10,13 @@ interface Props {
 
 const TimeAgo: FC<Props> = props => {
     const lastTick = useTicker()
-    const [timeAgo, setTimeAgo] = useState(lastTick.to(props.timestamp))
+    const [timeAgo, setTimeAgo] = useState<string>()
 
     useEffect(() => {
         setTimeAgo(lastTick.to(props.timestamp))
     }, [lastTick, props.timestamp])
 
-    return <Suspense fallback={timeAgo}>{timeAgo}</Suspense>
+    return timeAgo
 }
 
 export default TimeAgo
